6d59b6e5277aea271e764e168ea63e35f2002d74
Feroxpro <feliperamarcades@gmail.com>
2023-11-25 22:26:59 -0300

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Felipe Henrique Domingos on 25/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
